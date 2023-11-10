33846438aaeada65e6331330a6bc93e5f5348068
Julien Ripouteau <julien@ripouteau.com>
2023-11-10 20:09:18 +0100

    chore: update regexes jsdocs

--- src/regexes.ts
+++ src/regexes.ts
@@ -1,36 +1,32 @@
 /**
- * Used by edge linker
  * Find all the views that are being used inside an Edge template
  */
 export const edgeRegex = /(?<=@include\(['"]|@layout\(['"]|@!?component\(['"])([^'">]+)/g
 
 /**
- * Used by edge linker
- * Find all components as tags
+ * Find all components as tags inside an Edge template
  */
 export const edgeComponentsAsTagsRegex =
   /@!?(?!include|set|unless|let|eval|inject|!component|if|elseif|vite|entryPointScripts|entryPointStyles|each|click|section|layout|component|slot|!section)(.+)?\(?/g
 
 /**
- * Used by edge suggester
- * Check if we are currently inside a view link and capture the user input
+ * Find all the views that are being used inside a TS/Js file
+ */
+export const tsRegex = /(?:[Vv]iew|[Ee]dge)\.render(?:Sync)?\(['"](.*)['"]/g
+
+/**
+ * Check if we are currently inside a view link and capture the user input to suggest completions
  */
 export const viewsCompletionRegex = /(?<=[@include|@layout|@!component]\()(['"])[^'"]*\1/g
 
 /**
- * Used by edge linker
- * Find all components as tags
+ * Check if we are currently inside a component as tag and capture the user input
+ * to suggest completions
  */
 export const edgeComponentsAsTagsCompletionRegex =
   /@!?(?!include|end|set|unless|let|eval|inject|!component|each|section|layout|component)([\w.]+)/g
 
 /**
- * Used by view linker
- * Find all the views that are being used inside a TS code
- */
-export const tsRegex = /(?:[Vv]iew|[Ee]dge)\.render(?:Sync)?\(['"](.*)['"]/g
-
-/**
- * Check if we are currently inside a view link and capture the user input
+ * Check if we are currently inside a view link and capture the user input to suggest completions
  */
 export const tsViewsCompletionRegex = /(?<=([Ee]dge|[Vv]iew)\.render(?:Sync)?\()(['"])[^'"]*\2/g
